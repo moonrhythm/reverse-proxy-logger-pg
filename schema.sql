bf31a38759f8b66d6f580ec58470dcc6f1d7daef
Thanatat Tamtan <acoshift@gmail.com>
2023-08-09 15:36:09 +0700

    fix schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 create table request_logs (
     request  jsonb       not null,
     response jsonb       not null,
-    ts       timestamptz not null,
-    primary key (id)
-)
+    ts       timestamptz not null
+);
+create index request_logs_ts_idx on request_logs (ts);
